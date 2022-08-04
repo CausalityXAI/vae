979adc6f1a965b782838b9c1f2ef7b2a21141b0a
an-seunghwan <dpeltms79@gmail.com>
2022-08-04 19:16:44 +0900

    Update sweep.py

--- vqvae/sweep.py
+++ vqvae/sweep.py
@@ -12,7 +12,7 @@ except:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "wandb"])
     with open("../wandb_api.txt", "r") as f:
         key = f.readlines()
-    subprocess.run(["wandb", "login"], input=key[0], encoding='utf-8')
+    subprocess.run(["wandb", "login", "--relogin"], input=key[0], encoding='utf-8')
     import wandb
 #%%
 out = subprocess.run(
